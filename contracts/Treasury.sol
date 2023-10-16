6027d3ae3adb8b483f04206326d46ff9b8dc25ff
malcolmtan <zherui_tan@live.com.sg>
2023-10-16 14:40:06 +0800

    Update function role access and testnet address for PURSE var

--- contracts/Treasury.sol
+++ contracts/Treasury.sol
@@ -10,7 +10,8 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 contract Treasury is Initializable, UUPSUpgradeable, Governable {
     using SafeERC20 for IERC20;
 
-    address public constant PURSE = 0x29a63F4B209C29B4DC47f06FFA896F32667DAD2C;
+    //PURSE: 0x29a63F4B209C29B4DC47f06FFA896F32667DAD2C (mainnet)
+    address public constant PURSE = 0x337610d27c682E347C9cD60BD4b3b107C9d34dDd;
     address public PURSE_STAKING;
     uint256 public depositHistoricTotal;
     uint256 public disburseHistoricTotal;
@@ -32,10 +33,12 @@ contract Treasury is Initializable, UUPSUpgradeable, Governable {
         PURSE_STAKING = 0xFb1D31a3f51Fb9422c187492D8EA14921d6ea6aE;
         disburseInterval = 30 days;
         range = 12;
+
+        __UUPSUpgradeable_init();
     }
 
     /**************************************** ONLY GOVERNOR_ROLE & OPERATOR_ROLE FUNCTIONS ****************************************/
-    //Governor
+    //Governor only
     function depositPurseToTreasury(uint256 _amount) external onlyRole(GOVERNOR_ROLE) {
         require(_amount > 0, "Amount must be more than 0");
         require(
@@ -46,8 +49,8 @@ contract Treasury is Initializable, UUPSUpgradeable, Governable {
         depositHistoricTotal += _amount;
         emit DepositPurseToTreasury(msg.sender, block.timestamp, _amount);
     }
-    //Operator
-    function governorDisburseToPurseStaking(uint256 _reqAmount) external onlyRole(OPERATOR_ROLE) {
+    //Governor & Operator
+    function governorDisburseToPurseStaking(uint256 _reqAmount) external onlyRole(GOVERNOR_ROLE) onlyRole(OPERATOR_ROLE) {
         require(
             block.timestamp > lastDisbursementTimestamp + disburseInterval,
             "Disbursement interval not reached"
