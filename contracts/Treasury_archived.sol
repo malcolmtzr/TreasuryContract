b9f2f1a537621f39f1cdbb3eb6cd21a6599ee081
malcolmtan <zherui_tan@live.com.sg>
2023-10-16 14:37:56 +0800

    Remove artifact name conflict

--- contracts/Treasury_archived.sol
+++ contracts/Treasury_archived.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Treasury is Ownable {
+contract Treasury_Archived is Ownable {
     using SafeERC20 for IERC20;
 
     address public constant PURSE = 0x29a63F4B209C29B4DC47f06FFA896F32667DAD2C;
